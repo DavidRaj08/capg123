e810b5ab724b18485862adc311d14ac175d7fa21
dsamband <dsamband@DIN76000007.corp.capgemini.com>
2019-07-04 11:25:40 +0530

    Commit to local repository

---
+++ MyJenkinsDemo/src/main/java/com/cg/demo/MainClass.java
@@ -0,0 +1,12 @@
+package com.cg.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		SalesClass s =new SalesClass();
+		s.show();
+		
+	}
+
+}
---
+++ MyJenkinsDemo/src/main/java/com/cg/demo/SalesClass.java
@@ -0,0 +1,11 @@
+package com.cg.demo;
+
+public class SalesClass {
+
+	private int[] arr = {1, 3, 5, 9};
+	public void show() {
+		for(int num: arr) {
+			System.out.println(num);
+		}
+	}
+}
