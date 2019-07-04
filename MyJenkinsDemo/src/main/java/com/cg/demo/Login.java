defa2cddab6c26972b5d9f8e9b01ee54dcab0488
dsamband <dsamband@DIN76000007.corp.capgemini.com>
2019-07-04 15:17:25 +0530

    Updated Login class

--- MyJenkinsDemo/src/main/java/com/cg/demo/Login.java
+++ MyJenkinsDemo/src/main/java/com/cg/demo/Login.java
@@ -2,4 +2,28 @@ package com.cg.demo;
 
 public class Login {
 
+	private String password;
+
+	@Override
+	public String toString() {
+		return "Login [password=" + password + ", username=" + username + "]";
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	private String username;
 }
